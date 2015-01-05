1e54ab00c97959a6cca46ee555fc6c457a480140
Issaq Al-Ahmed <issaq_al-ahmed@outlook.com>
2015-01-05 11:22:57 -0800

    redefining headers

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ var app = express();
 var port = process.env.PORT || 3000;
 var url = process.env.URL || 'localhost';
 
-app.get('/', function(req, res){
-	//call the functions for the database here
-  res.send("derp");
+app.use(function(req, res, next) { 
+	res.header('Access-Control-Allow-Origin', '*'); 
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	if ('OPTIONS' == req.method) {
+		res.send(200); 
+	} 
+	else { 
+		next(); 
+	} 
 });
 
 app.get('/api/emailList', function(req, res){
 	//call the functions for the database here
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	// res.setHeader("Access-Control-Allow-Origin", "*");
   	res.status(200).send(emailList);
 });
 
 app.get('/api/emails/:id', function(req, res){
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	// res.setHeader("Access-Control-Allow-Origin", "*");
 	// var thing = req);
 	// var test = {stuff: 'lol'};
 	//call the functions for the database here
